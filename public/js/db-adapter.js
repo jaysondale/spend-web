5d53c29f42b3f0a5cceaeb0a6f791d5a48b206be
Jayson Dale <jaysondale1@gmail.com>
2020-06-03 13:47:15 -0400

    Created database adapter and tested getTransactions function

--- public/js/db-adapter.js
+++ public/js/db-adapter.js
@@ -2,6 +2,35 @@
 const db = firebase.firestore();
 
 // Get transactions for given user and category
-let getTransactions = function(usrID, category) {
+let getTransactions = async function(usrID, category) {
+    // Get user ref
+    let usrRef = db.collection(usrID);
 
+    // Get maps
+    return await usrRef.doc("id_map").get().then(async docSnap => {
+        if (docSnap) {
+            let id_map = docSnap.data();
+            // Get transactions
+            return await usrRef.doc("transactions").get().then(docSnap => {
+                let transactions = docSnap.data();
+                let filteredTransactions = new Map();
+                // Filter out transactions
+                for (let [key, value] of Object.entries(transactions)) {
+                    let id = value["ID"];
+                    if (id in id_map){
+                        if (id_map[id] === category) {
+                            filteredTransactions.set(key, value);
+                        }
+                    }
+                }
+                return filteredTransactions;
+            })
+        } else {
+            return -1;
+        }
+    })
 };
+
+(async () => [
+    console.log(await getTransactions("user_1", "Food"))
+])();
